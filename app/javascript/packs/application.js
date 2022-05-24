da5925aba82e402e056c435507b932c8a7561e4b
Katia M <katy.chaparro.2010@gmail.com>
2022-05-24 12:46:47 -0400

    Add de js file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
 import 'bootstrap';
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+require.context('../images',true)
+
