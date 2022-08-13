755af061f8865ec92ca877468b342d8e4fafce21
fatimaTalavera <fa.talavera95@gmail.com>
2022-08-13 00:53:21 -0400

    fix bootstrap js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,10 +7,20 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap';
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import { Toast } from "bootstrap"
+
+document.addEventListener("turbolinks:load", () => {
+  let toastElList = [].slice.call(document.querySelectorAll('.toast'))
+  let toastList = toastElList.map(function (toastEl) {
+    return new Toast(toastEl)
+  })
+  toastList.forEach(toast => toast.show())
+});
+
 
