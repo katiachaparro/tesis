547d86f88bc25f39f65b223dace42a86f789b3f7
fatimaTalavera <fa.talavera95@gmail.com>
2022-08-23 16:08:10 -0400

    change readme and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ r1 = Role.create({ name: 'User'})
 r2 = Role.create({ name: 'Admin'})
 r3 = Role.create({ name: 'Super-admin'})
 
-User.create({ email: 'admin@admin.com', password: '12345qQ!', password_confirmation: '12345qQ!', role_id: r1.id })
+User.create({ first_name:'admin', last_name:'admin' email: 'admin@admin.com', password: '12345qQ!', password_confirmation: '12345qQ!', role_id: r1.id })
